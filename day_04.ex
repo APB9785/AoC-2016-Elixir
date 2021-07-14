42421a12dd78fe6e8b01fd571c3d23e34f665ab0
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-07-14 18:02:29 -0500

    Day 4 Setup

---
+++ day_04.ex
@@ -0,0 +1,14 @@
+defmodule Day4 do
+  def main do
+    with input <- parse_input() do
+      IO.puts([
+        "**********************\nADVENT OF CODE - DAY 4\n**********************",
+        "\nPart 1: ???",
+        "\nPart 2: ???",
+        "\n"
+      ])
+    end
+  end
+end
+
+Day4.main()
