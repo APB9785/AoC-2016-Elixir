1b54adb54fa5428d802706806af8ac089d0968ea
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-07-14 01:47:41 -0500

    Setup Day 3

---
+++ day_03.ex
@@ -0,0 +1,12 @@
+defmodule Day3 do
+  def main do
+    IO.puts([
+      "**********************\nADVENT OF CODE - DAY 3\n**********************\n",
+      "Part 1: ???",
+      "\nPart 2: ???",
+      "\n"
+    ])
+  end
+end
+
+Day3.main()
