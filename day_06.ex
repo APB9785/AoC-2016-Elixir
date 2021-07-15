a47b5c090b880a337812797f42e5f1aa095ff3db
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-07-15 13:58:50 -0500

    Day 6 setup

---
+++ day_06.ex
@@ -0,0 +1,15 @@
+defmodule Day6 do
+  @spec main() :: :ok
+  def main do
+    with input <- parse_input() do
+      IO.puts([
+        "**********************\nADVENT OF CODE - DAY 6\n**********************",
+        "\nPart 1: ???",
+        "\nPart 2: ???",
+        "\n"
+      ])
+    end
+  end
+end
+
+Day6.main()
