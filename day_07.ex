5a6d37e64a15288071879dc65e61d7ea2335edcc
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-07-16 16:27:29 -0500

    Day 7 Part 2

--- day_07.ex
+++ day_07.ex
@@ -9,8 +9,9 @@ defmodule Day7 do
       IO.puts([
         "**********************\nADVENT OF CODE - DAY 7\n**********************",
         "\nPart 1: ",
-        part_1(input),
-        "\nPart 2: ???",
+        check_all(input, :tls),
+        "\nPart 2: ",
+        check_all(input, :ssl),
         "\n"
       ])
     end
@@ -22,17 +23,26 @@ defmodule Day7 do
     |> String.split("\n", trim: true)
   end
 
-  @spec part_1(list(binary())) :: binary()
-  def part_1(input) do
+  @spec check_all(list(binary()), atom()) :: binary()
+  def check_all(input, :tls) do
     input
-    |> Stream.map(&check_line/1)
+    |> Stream.map(&check_line_tls/1)
     |> Enum.count(&(&1.found_outside and not &1.found_inside))
     |> Integer.to_string()
   end
 
-  @spec check_line(binary()) :: state()
-  def check_line(line) do
-    parse(%{
+  def check_all(input, :ssl) do
+    input
+    |> Stream.map(&check_line_ssl/1)
+    |> Enum.count(&compare_matches/1)
+    |> Integer.to_string()
+  end
+
+  ## PART 1 - TLS
+
+  @spec check_line_tls(binary()) :: state()
+  def check_line_tls(line) do
+    parse_tls(%{
       todo: String.graphemes(line),
       inside_now: false,
       found_outside: false,
@@ -40,32 +50,95 @@ defmodule Day7 do
     })
   end
 
-  @spec parse(state()) :: state()
-  def parse(%{todo: [a, b, b, a]} = state)
+  @spec parse_tls(state()) :: state()
+  def parse_tls(%{todo: [a, b, b, a]} = state)
       when a != b and a not in @brackets and b not in @brackets do
     Map.put(state, :found_outside, true)
   end
 
-  def parse(%{todo: [_, _, _, _]} = state), do: state
+  def parse_tls(%{todo: [_, _, _, _]} = state), do: state
 
-  def parse(%{todo: [a, b, b, a | _]} = state)
+  def parse_tls(%{todo: [a, b, b, a | _]} = state)
       when a != b and a not in @brackets and b not in @brackets do
     if state.inside_now do
-      state |> Map.update!(:todo, &tl/1) |> Map.put(:found_inside, true) |> parse()
+      state |> Map.update!(:todo, &tl/1) |> Map.put(:found_inside, true) |> parse_tls()
     else
-      state |> Map.update!(:todo, &tl/1) |> Map.put(:found_outside, true) |> parse()
+      state |> Map.update!(:todo, &tl/1) |> Map.put(:found_outside, true) |> parse_tls()
     end
   end
 
-  def parse(%{todo: [_, _, _, "[" | _]} = state) do
-    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, true) |> parse()
+  def parse_tls(%{todo: [_, _, _, "[" | _]} = state) do
+    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, true) |> parse_tls()
   end
 
-  def parse(%{todo: [_, _, _, "]" | _]} = state) do
-    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, false) |> parse()
+  def parse_tls(%{todo: [_, _, _, "]" | _]} = state) do
+    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, false) |> parse_tls()
   end
 
-  def parse(state), do: state |> Map.update!(:todo, &tl/1) |> parse()
+  def parse_tls(state), do: state |> Map.update!(:todo, &tl/1) |> parse_tls()
+
+  ## PART 2 - SSL
+
+  @spec check_line_ssl(binary()) :: state()
+  def check_line_ssl(line) do
+    parse_ssl(%{
+      todo: String.graphemes(line),
+      inside_now: false,
+      found_outside: [],
+      found_inside: []
+    })
+  end
+
+  @spec parse_ssl(state()) :: state()
+  def parse_ssl(%{todo: [a, b, a]} = state)
+      when a != b and a not in @brackets and b not in @brackets do
+    Map.update!(state, :found_outside, &[Enum.join([a, b, a]) | &1])
+  end
+
+  def parse_ssl(%{todo: [_, _, _]} = state), do: state
+
+  def parse_ssl(%{todo: [a, b, a | _]} = state)
+      when a != b and a not in @brackets and b not in @brackets do
+    if state.inside_now do
+      state
+      |> Map.update!(:todo, &tl/1)
+      |> Map.update!(:found_inside, &[Enum.join([a, b, a]) | &1])
+      |> parse_ssl()
+    else
+      state
+      |> Map.update!(:todo, &tl/1)
+      |> Map.update!(:found_outside, &[Enum.join([a, b, a]) | &1])
+      |> parse_ssl()
+    end
+  end
+
+  def parse_ssl(%{todo: [_, _, "[" | _]} = state) do
+    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, true) |> parse_ssl()
+  end
+
+  def parse_ssl(%{todo: [_, _, "]" | _]} = state) do
+    state |> Map.update!(:todo, &tl/1) |> Map.put(:inside_now, false) |> parse_ssl()
+  end
+
+  def parse_ssl(state), do: state |> Map.update!(:todo, &tl/1) |> parse_ssl()
+
+  @spec compare_matches(Enumerable.t()) :: boolean()
+  def compare_matches(state) do
+    Enum.reduce_while(state.found_inside, false, fn bab, _acc ->
+      if invert(bab) in state.found_outside do
+        {:halt, true}
+      else
+        {:cont, false}
+      end
+    end)
+  end
+
+  @spec invert(binary()) :: binary()
+  defp invert(input) do
+    with [a, b, a] <- String.graphemes(input) do
+      Enum.join([b, a, b])
+    end
+  end
 end
 
 Day7.main()
